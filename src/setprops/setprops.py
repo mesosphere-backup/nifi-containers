bb78810d4479eb3d9c4bd396583423aa2f3ede5b
Martijn Dekkers <mdekkers@mesosphere.com>
2019-09-26 04:38:23 +0200

    cleaning up tools

--- src/setprops/setprops.py
+++ src/setprops/setprops.py
@@ -17,9 +17,10 @@ def cli():
 @click.option(
     '--prefix',
     '-p',
-    help='What prefix to use. Will also use the value of the CNF_PREFIX environment variable, if defined. Default prefix is DCNF_',
+    help='What prefix to use. Will also use the value of the CNF_PREFIX environment variable, '
+         'if defined. Default prefix is DCNF_',
     required=True,
-    default = 'DCNF_',
+    default='NF_',
     envvar='CNF_PREFIX'
 )
 @click.option(
@@ -31,54 +32,47 @@ def cli():
     envvar='CNF_TEMPL'
 )
 @click.option(
+    '--templatedir',
+    '-d',
+    help='Directory where templates live',
+    required=True
+)
+@click.option(
     '--configfile',
     '-c',
     required=True,
     type=click.File('w'),
     envvar='CNF_CONF',
-    help='Config file to generate. Can be used multiple times. '                                                                                                            
+    help='Config file to generate. Can be used multiple times. '
          'Will use the value of the CNF_CONF environment variable, if defined.'
 )
-@click.option(
-    '--keysplit',
-    '-k',
-    help='Key split character to use. Many config files will use some form of key seperator '
-         'such as "this.is.a.key" that don\'t always translate nicely in environment variables'
-         'Always use the underscore in your environment variables, and use this switch to'
-         'indicate what seperator to replace the underscore with.'
-         'Will also use the value of the CNF_KSPL environment variable, if defined.',
-    envvar='CNF_KSPL'
-)
 @click.version_option(
-    version=0.1,
+    version=1.0,
     prog_name='setprops',
     message='\nsetprops builds a configuration file from a JINJA2 template. For usage, see --help\n'
 )
-def mkiniconf(configfile,prefix,template):
+def mkiniconf(configfile, prefix, template, templatedir):
     """This program will read prefixed environment variables and a JINJA2 configuration file template,
     and combines them to generate a config file for an application or service."""
 
-    file_loader = FileSystemLoader('templates')
+    file_loader = FileSystemLoader(templatedir)
     env = Environment(loader=file_loader)
-
     thetemplate = env.get_template(template)
-    TheConfItems = {}
+    theconfitems = {}
+
+    env.filters['env_override'] = env_override
 
     for k in sorted(os.environ.keys()):
         v = os.environ[k]
         if not k.startswith(prefix):
             continue
-
         k = k[len(prefix):]
         k = k.lower()
         click.echo('[%s] Setting config %s to %s' % (configfile, k, v))
+        theconfitems[k] = v
 
-        TheConfItems[k] = v
-
-    TheConfigFile = thetemplate.render(TheConfItems)
-    configfile.write(TheConfigFile)
-
-    return
+    theconfigfile = thetemplate.render(theconfitems)
+    configfile.write(theconfigfile)
 
 
 @cli.command()
@@ -87,7 +81,7 @@ def mkiniconf(configfile,prefix,template):
     '-t',
     type=click.File('w'),
     required=True,
-    help='Template config file to create.',
+    help='Template config file to create.'
 )
 @click.option(
     '--configfile',
@@ -99,47 +93,47 @@ def mkiniconf(configfile,prefix,template):
 @click.option(
     '--kvseperator',
     '-s',
-    default="=",
+    default='=',
     help='What character separates keys from values. Defaults to "="'
 )
 @click.option(
     '--keysplit',
     '-k',
-    default=".",
+    default='.',
     help='Key split character to use. Many config files will use some form of key seperator '
-         'such as "this.is.a.key" that don\'t always translate nicely in environment variables'
-         'Always use the underscore in your environment variables, and use this switch to'
-         'indicate what seperator to replace the underscore with. Defaults to "."')
+         'such as "this.is.a.key" that don\'t always translate nicely in environment variables. '
+         'Always use the underscore in your environment variables, and use this switch to '
+         'indicate what seperator to replace the underscore with. Defaults to "."'
+)
 @click.option(
     '--comment',
     '-m',
-    default="#",
+    default='#',
     help='What character denotes a linecomment. Defaults to "#"'
 )
-def mktemplate(template,configfile,kvseperator,keysplit,comment):
+def mktemplate(template, configfile, kvseperator, keysplit, comment):
     lines = []
-
     for line in configfile:
         lines.append(line)
         line = line.strip()
         if not line or line.startswith(comment):
-            template.write(line + "\n")
+            template.write(line + '\n')
             continue
+            k, v = line.split(kvseperator, 1)
+            thekey = k.replace(keysplit, '_')
+            if v is not '':
+                v = '{{ ' + thekey.lower() + '|default("' + v + '") }}'
+            else:
+                v = '{{ ' + thekey.lower() + ' }}'
+            template.write(k + '=' + v + '\n')
 
-        k, v = line.split(kvseperator, 1)
-        thekey = k.replace(keysplit, "_")
-        if v is not "":
-            v = "{{ " + thekey.lower() + "|default(\"" + v + "\") }}"
-        else:
-            v = "{{ " + thekey.lower() + " }}"
 
-        template.write(k + "=" + v +"\n")
-
-    return
+def env_override(value, key):
+    return os.getenv(key, value)
 
 
 def main():
     cli()
-    return
+
 
 main()
