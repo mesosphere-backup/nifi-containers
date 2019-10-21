42e66626869607e44fd0bc3ec1ff5c7a07eca403
Martijn Dekkers <mdekkers@mesosphere.com>
2019-10-21 11:03:35 +0200

    Minor updates

--- nifi-main/sh/start.sh
+++ nifi-main/sh/start.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# Copyright (c) 2019 Martijn Dekkers, D2iQ.
+# Licensed under the Apache 2.0 License
+# Martijn Dekkers <mdekkers@d2iq.com>
+
 
 # Load script variables
 scripts_dir='/opt/nifi/scripts'
